1dcf969a5301c00e4c8efb6ce0242c34a509f030
ChezVan <chezney@thedigitalacademy.co.za>
2023-02-09 18:33:47 +0200

    add div, h2 and Product.map

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function Main() {
+function Main(props) {
+  const {prodcts} = props
   return (
-    <div className='block col-2'>Main</div>
+    <div className='block col-2'>
+      <h2> Products</h2>
+      <div className="row">
+        {prodcts.map((products) => (
+          <div>{prodcts.name}</div>
+        ))}
+      </div>
+    </div>
   )
 } 
 
